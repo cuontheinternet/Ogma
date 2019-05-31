d466575bc5eb14bb3fe23f8cdd1b268a1f9a475e
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2019-05-31 04:23:46 +0100

    Use a different trie implementation

--- src/util/IconUtil.js
+++ src/util/IconUtil.js
@@ -4,9 +4,8 @@
  * @license GPL-3.0
  */
 
-import TrieSearch from 'trie-search';
-
 import {ColorsLight} from '../typedef';
+import ExactTrie from '../../../shared/ExactTrie';
 
 export const FolderIconData = {name: 'folder', colorCode: 0};
 const FileIconData = {name: 'file', colorCode: 0};
@@ -16,28 +15,28 @@ const IconsToExtensions = {
     'file-archive': ['zip', 'rar', 'tar', 'tar.gz'],
     'file-image': ['png', 'jpg', 'jpeg', 'svg', 'tiff'],
     'file-pdf': ['pdf'],
-    'film': ['avi', 'mp4', 'mkv', 'flv'],
+    'film': ['avi', 'mp4', 'mkv', 'flv', 'webm'],
     'file-code': ['js', 'html', 'php'],
     'terminal': ['run', 'sh'],
 
     'b:ubuntu': ['deb'],
+    'b:linux': ['AppImage'],
 };
 
 const step = 7;
 let colourIndex = 0;
 
-const ts = new TrieSearch();
+const exactTrie = new ExactTrie();
 for (const name in IconsToExtensions) {
     const exts = IconsToExtensions[name];
     for (let i = 0; i < exts.length; ++i) {
         colourIndex += step;
         const colorCode = colourIndex % (ColorsLight.length - 1) + 1;
-        ts.map(exts[i], {name, colorCode});
+        exactTrie.put(exts[i], {name, colorCode}, true);
     }
 }
 
 export const getIconData = file => {
-    const ext = file.ext.replace(/^\./, '');
-    const match = ts.get(ext);
-    return match.length === 0 ? FileIconData : match[0];
+    const match = exactTrie.getWithCheckpoints(file.base, '.', true);
+    return match ? match : FileIconData;
 };
